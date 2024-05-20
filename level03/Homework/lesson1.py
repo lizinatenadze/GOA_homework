33f5a08dd5bba1ed70b747d8950903ac74cf6f0a
lizinatenadze <elisabedinatenadze@gmail.com>
2024-05-20 22:43:21 +0400

    first commit

---
+++ level03/Homework/lesson1.py
@@ -0,0 +1,6 @@
+name = "lizi"
+last_name = "natenadze"
+age = 13
+hoby = "cekva"
+country = "saqartvelo"
+print(name, last_name + age, hoby, country)
---
+++ level03/Homework/lesson2.py
@@ -0,0 +1,6 @@
+name = "lizi"
+last_name = "natenadze"
+age = 13
+hoby = "cekva"
+country = "saqartvelo"
+print("me var" + " " + name + " " + last_name + " " + str(age) + " " + "wlis" + " " + "me miyvars" + " " + hoby + " " + "da vcxovrob" + " " + country  + "_shi")
