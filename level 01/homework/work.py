da8f170a9935d6f3e1e0cbe3d116c8d997f5245e
lizinatenadze <elisabedinatenadze@gmail.com>
2024-06-23 19:07:25 +0400

    second commit

---
+++ level 01/homework/work.py
@@ -0,0 +1,10 @@
+name="lizi"
+surname="natenadze"
+
+height=168
+age=13
+weight=58
+
+home_town="Rustavi, Gorgia "
+
+print("my name is"+" "+name+" "+surname+" "+"."+"I am "+str(age)+"years old"+". "+" "+"i am from"+" "+home_town+". "+" my height is"+" "+str(height)+"cm  "+"and i weigh"+" "+str(weight))
