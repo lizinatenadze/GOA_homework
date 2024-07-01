f5af05068cba749cfbf0d45b701ff0a22b911d61
lizinatenadze <elisabedinatenadze@gmail.com>
2024-07-01 12:17:57 +0400

    second commit

---
+++ level 13/classswork/work.py
@@ -0,0 +1,7 @@
+# the program takes a core as an input
+core1 = int(input( 'enter the first core: '))
+
+# add the comparison operation inside the parentheses
+core2 = int(input('enter the secnt core:'))
+
+print( core2 and core1 )
