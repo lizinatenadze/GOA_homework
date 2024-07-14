716d0ed4e07e3290c474ed7d0432f0ec9e3c2aa3
lizinatenadze <elisabedinatenadze@gmail.com>
2024-07-14 23:40:19 +0400

    jjj

---
+++ level 17/homework/work.py
@@ -0,0 +1,16 @@
+#for loop-ის საშუალებით ტერმინალში გამოიტანეთ 30-ჯერ "GOA IS THE BEST"
+for i in range(31):
+    print("GOA IS THE BEST")
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ level 17/homework/work7.py
@@ -0,0 +1,8 @@
+# შექმენით პროგრამა სადაც გამოიყენებთ While loop - ს. თქვენი დავალება იქნება ის, რომ მომხამრებელს შემოატანინოთ რიცხვი და თქვენ უნდა გამოიცნოთ ეს რიცხვი, ხოლო ყოველ არ გამოცნობილ რიცხვზე ისევ თავიდან უნდა შეგეკითხოთ და შეიყვანოთ რიცხვი.
+
+num = int(input('enter number: '))
+guess_number = int(input("guess the number: "))
+
+
+while num != guess_number:
+      guess_number_again = int(input("guess the number: "))
