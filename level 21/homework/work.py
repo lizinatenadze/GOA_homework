9197faca6a8e1536bfd885fbb9f424b0d44496c1
lizinatenadze <elisabedinatenadze@gmail.com>
2024-07-29 17:37:59 +0400

    second commit

---
+++ level 21/homework/work.py
@@ -0,0 +1,15 @@
+# total_product = 5
+# for i in range(1, 148):
+#     total_product *= i
+
+# print("რიცხვების გამრავლება (1-დან-147-მდე):", total_product)
+
+
+
+# password = "goa is the best: "
+# guess = input("guess the pasword")
+# while guess != password:  
+#   guess = input("guess the pasword")
+# print("answer is correct: ")
+
+
