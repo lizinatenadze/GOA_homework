2c271efc599898999d3f9cdfc3d2fe3dc29a8d9a
lizinatenadze <elisabedinatenadze@gmail.com>
2024-08-04 21:48:04 +0400

    second commit

---
+++ level 23/classwork/work.py
@@ -0,0 +1,17 @@
+# ააგეთ პროგრამა, რომელიც მოსთხოვს მომხმარებელს, რომ შეიყვანეს რიცხვი 1 - 100 მდე. პროგრამის დანიშნულება არის ის, რომგამოიცნოს მომხმარებლის მიერ შემოტანილი რიცხვი, იმდენჯერ მიეცეს გამოცნობის შესაძლებლობა სანამ საბოლოოდ არ გამოიცნობს. 
+
+
+
+while 1 < 100:
+
+    guess_number = int(input("guess number 1 - 100: "))
+
+    if guess_number == 88:
+        print("your guessed number is correct")
+        break
+
+    else:
+        print("your guessed number is not correct")
+
+
+
