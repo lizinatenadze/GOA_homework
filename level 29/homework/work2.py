dad87374ed473790b3dd15ed44a0bc2325d58c20
lizinatenadze <elisabedinatenadze@gmail.com>
2024-08-21 19:32:32 +0400

    second commit

---
+++ level 29/homework/work2.py
@@ -0,0 +1,9 @@
+# შექმენი სია, სადაც ჩაწერთ თამაშების სახელებს, და შემდეგ ამ თამაშების სახელებს ჩანაცვლებთ პროგრამირების ენებით, და შემდეგ დაპრინტავთ ამ სიას.
+
+games = ["The Legend of Zelda", "Minecraft", "Among Us", "Fortnite", "Call of Duty"]
+
+programminglanguages = ["Python", "JavaScript", "Java", "html", "css"]
+
+games = programminglanguages
+
+print(games)
