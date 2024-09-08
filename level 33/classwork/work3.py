d1d91fdacd4e801b745797d5d260adfc5e53fe24
lizinatenadze <elisabedinatenadze@gmail.com>
2024-09-08 23:41:44 +0400

    second commit

---
+++ level 33/classwork/work3.py
@@ -0,0 +1,8 @@
+ #შექმენით ფუნქცია, რომელიც სიყვარულის წერილს მიუძღვნის მომხმარებელს:
+# ტექსტი თქვენ მოიფიქრეთ.
+# ტექსტის ბოლოს აუცილებლად ჩაამატეთ ეს ნაწილი: 'სიყვარულით {სახელი}'
+
+def loveee(name):
+    print("dear", name, "You are everything in my life, your smile, smell, and everything is priceless.")
+
+loveee('human')
